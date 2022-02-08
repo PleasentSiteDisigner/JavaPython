9078fb684aef1b105219643a80a7ebec4123fab7
Yurii <yuriipanchenko@gmail.com>
2022-02-08 11:44:26 +0700

    Lesson 3 task 2

--- Lesson3/src/Task2.java
+++ Lesson3/src/Task2.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Task2 {
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 8, x = 2, y = 10;
+        int factorial = getFactorial(a);
+        int powerX = power(x ,y);
+        System.out.println(factorial);
+        System.out.println(powerX);
+    }
+
+    public static int getFactorial(int x) {
+        if (x == 1) {
+            return 1;
+        } else {
+            return x * getFactorial(x - 1);
+        }
+    }
+    public static int power(int x, int y) {
+        if (y == 1) {
+            return x;
+        } else {
+            return x * power(x, y - 1);
+        }
+    }
 }
